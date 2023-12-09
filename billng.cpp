51f3d8e9e6171f4c09d57d2b811325a1154fb694
Sagar <sagarsugunan7902@gmail.com>
2023-12-09 11:34:47 +0530

    Your commit message here

---
+++ billng.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+class shopping 
+{
+    private:
+        int pcode;
+        float price;
+        float dis;
+    public:
+        void menu();
+        void administrator();
+        void buyer();
+        
+    
+}
